138583ee93edb93e25c1aef8b61d954ff63e6422
vasaikarSimii <vasaikar.s@northeastern.edu>
2020-09-29 22:43:56 +0530

    Assignment1 Submission

---
+++ Assignment1/src/Business/Person.java
@@ -0,0 +1,261 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.awt.Image;
+import javax.swing.Icon;
+
+/**
+ *
+ * @author Seemin
+ */
+public class Person {
+    
+    @Override
+    public String toString()
+    {
+        return this.date;
+        //return this.name;
+    }
+  
+    
+    private String name;
+    private String date;
+    private String geographicalData;
+    private int faxNo;
+    private long telPhn1;
+    private long telPhn2;
+    private int ssnNo;
+    private String email1;
+    private String email2;
+    private int medNo;
+    private int hpbNo;
+    private long bankacc1;
+    private long bankacc2;
+    private String linkedin;
+    private int license;
+    private long ipAdd;
+    private int vehicleID;
+    private int licensePlateNo;
+    private int deviceID;
+    private int serialNo;
+    private String bioID;
+    private String uniqueID;
+    private Icon photo1;
+    private Icon photo2;
+
+    public Icon getPhoto1() {
+        return photo1;
+    }
+
+    public void setPhoto1(Icon photo1) {
+        this.photo1 = photo1;
+    }
+
+    public Icon getPhoto2() {
+        return photo2;
+    }
+
+    public void setPhoto2(Icon photo2) {
+        this.photo2 = photo2;
+    }
+
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getGeographicalData() {
+        return geographicalData;
+    }
+
+    public void setGeographicalData(String geographicalData) {
+        this.geographicalData = geographicalData;
+    }
+
+    public int getFaxNo() {
+        return faxNo;
+    }
+
+    public void setFaxNo(int faxNo) {
+        this.faxNo = faxNo;
+    }
+
+    public long getTelPhn1() {
+        return telPhn1;
+    }
+
+    public void setTelPhn1(long telPhn1) {
+        this.telPhn1 = telPhn1;
+    }
+
+    public long getTelPhn2() {
+        return telPhn2;
+    }
+
+    /**
+     *
+     * @param telPhn2
+     */
+    public void setTelPhn2(long telPhn2) {
+        this.telPhn2 = telPhn2;
+    }
+
+    public int getSsnNo() {
+        return ssnNo;
+    }
+
+    public void setSsnNo(int ssnNo) {
+        this.ssnNo = ssnNo;
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public void setEmail1(String email1) {
+        this.email1 = email1;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public void setEmail2(String email2) {
+        this.email2 = email2;
+    }
+
+    public int getMedNo() {
+        return medNo;
+    }
+
+    public void setMedNo(int medNo) {
+        this.medNo = medNo;
+    }
+
+    public int getHpbNo() {
+        return hpbNo;
+    }
+
+    public void setHpbNo(int hpbNo) {
+        this.hpbNo = hpbNo;
+    }
+
+    public long getBankacc1() {
+        return bankacc1;
+    }
+
+    public void setBankacc1(long bankacc1) {
+        this.bankacc1 = bankacc1;
+    }
+
+    public long getBankacc2() {
+        return bankacc2;
+    }
+
+    public void setBankacc2(long bankacc2) {
+        this.bankacc2 = bankacc2;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public int getLicense() {
+        return license;
+    }
+
+    public void setLicense(int license) {
+        this.license = license;
+    }
+
+    public long getIpAdd() {
+        return ipAdd;
+    }
+
+    public void setIpAdd(long ipAdd) {
+        this.ipAdd = ipAdd;
+    }
+
+    public int getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(int vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public int getLicensePlateNo() {
+        return licensePlateNo;
+    }
+
+    public void setLicensePlateNo(int licensePlateNo) {
+        this.licensePlateNo = licensePlateNo;
+    }
+
+    public int getDeviceID() {
+        return deviceID;
+    }
+
+    public void setDeviceID(int deviceID) {
+        this.deviceID = deviceID;
+    }
+
+    public int getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(int serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getBioID() {
+        return bioID;
+    }
+
+    public void setBioID(String bioID) {
+        this.bioID = bioID;
+    }
+
+    public String getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+    
+             
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+            
+}
